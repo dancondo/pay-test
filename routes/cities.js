f56229b130a03709e7e5fd5b72c60a6bb505eca2
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-20 19:52:10 -0300

    Simple descriptor to the /cities routes

--- routes/cities.js
+++ routes/cities.js
@@ -2,6 +2,23 @@ const router = require('express').Router();
 
 const citiesController = require('../controllers/cities_controller');
 
+/**
+ * @swagger
+ * 
+ * /cities:
+ *   get:
+ *     description: Get all Cities
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Latitude
+ *         description: Latitude of a city
+ *         required: false
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: City list, or single city.
+ */
 router.get('/', citiesController.index)
 
 module.exports = {
