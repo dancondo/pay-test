52f10978558cbd9eb6ed5fb29d3ba7cd9b4fb322
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-20 19:55:13 -0300

    add dotenv and body parser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const citiesRoutes = require('./routes/cities');
-const docs = require('./routes/docs');
+const docsRoutes = require('./routes/docs');
 
+app.use(bodyParser.json())
 app.use(citiesRoutes.namespace, citiesRoutes.router);
-app.use(docs.namespace, docs.router);
+app.use(docsRoutes.namespace, docsRoutes.router);
 
-app.listen(3000);
+const port = process.env.PORT || 3000
+const host = process.env.HOST || '127.0.0.1'
+
+app.listen(port, host);
