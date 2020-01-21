57e7da89c130cc2056305665617d15ff3ae707b5
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-21 12:47:20 -0300

    Creates cities/:id route and add swagger descriptor

--- routes/cities.js
+++ routes/cities.js
@@ -20,7 +20,30 @@ const citiesController = require('../controllers/cities_controller');
  *       200:
  *         description: City list.
  */
-router.get('/', citiesController.index)
+router.get('/', citiesController.index);
+
+/**
+ * @swagger
+ * 
+ * /cities/{id}:
+ *   get:
+ *     description: Get a city by id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The id of the city you are searching for
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A City and its weather
+ *       404:
+ *         description: Couldn't find a city
+ */
+
+router.get('/:id', citiesController.show);
 
 module.exports = {
     router,
