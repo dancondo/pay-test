6ebaea14c281f236e79631db5b7834a4df503e9a
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-20 23:32:26 -0300

    Creates cities controller index method

--- controllers/cities_controller.js
+++ controllers/cities_controller.js
@@ -1,5 +1,9 @@
+const citiesService = require('../services/cities');
+
 exports.index = (req, res, next) => {
+    const query = req.query
+    const onlyWithWeather = query.only_with_weather && JSON.parse(query.only_with_weather)
     return res.json({
-        cities: ['Rio', 'SP']
+        cities: citiesService.findAll(onlyWithWeather)
     })
 }
