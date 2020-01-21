a2daefad5bab93137f4bd6f02972b4406f467944
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-20 23:31:39 -0300

    Create cities services with findAll method and City, Weather and Weather Details documentation

---
+++ services/cities.js
@@ -0,0 +1,71 @@
+const { cityList, weatherList } = require('../data/index.js');
+
+/**
+ * 
+ * @swagger
+ * 
+ * definitions:
+ *  City:
+ *    type: object
+ *    properties:
+ *      name:
+ *        type: string
+ *      country:
+ *        type: string
+ *      weather:
+ *        allOf:
+ *          - $ref: '#/definitions/Weather'
+ *
+ *  Weather:
+ *    type: object
+ *    properties:
+ *      dateTime:
+ *        type: string
+ *      details:
+ *        type: array
+ *        items:
+ *          allOf:
+ *            - $ref: '#/definitions/WeatherDetails'
+ * 
+ *  WeatherDetails:
+ *    type: object
+ *    properties:
+ *      name:
+ *        type: strin
+ *      description:
+ *        type: string
+ *  
+ */
+const cityDTO = (city) => {
+    return {
+        name: city.name,
+        country: city.country,
+        weather: city.weathers ? city.weathers.map(weather => weatherDTO(weather)) : undefined
+    }
+}
+
+const weatherDTO = (weather) => {
+    return {
+        dateTime: weather.dt,
+        details: weather.weather.map(w => ({
+            name: w.main,
+            description: w.description
+        }))
+    }
+}
+
+exports.findAll = (onlyWithWeather) => {
+    const cities = onlyWithWeather
+        ? cityList.reduce((arr, city) => {
+            const weather = weatherList.find(weather => weather.cityId === city.id)
+            if (weather) {
+                arr.push({
+                    ...city,
+                    weathers: weather.data
+                })
+            }
+            return arr
+        }, [])
+        : cityList
+    return cities.map(city => cityDTO(city));
+}
