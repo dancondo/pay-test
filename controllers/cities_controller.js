f44b5993b0618e3c00a237d6979b4d1fc9e3888b
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-21 12:46:22 -0300

    Add status codes to responses and creates cities controller show method

--- controllers/cities_controller.js
+++ controllers/cities_controller.js
@@ -3,7 +3,14 @@ const citiesService = require('../services/cities');
 exports.index = (req, res, next) => {
     const query = req.query
     const onlyWithWeather = query.only_with_weather && JSON.parse(query.only_with_weather)
-    return res.json({
+    return res.status(200).json({
         cities: citiesService.findAll(onlyWithWeather)
+    })
+}
+
+exports.show = (req, res, next) => {
+    const id = JSON.parse(req.params.id)
+    return res.status(200).json({
+        city: citiesService.findById(id)
     })
 }
