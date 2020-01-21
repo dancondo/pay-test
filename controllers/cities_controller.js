aa9583a9f3f09f7d8b1cf77f75beea9f3eabd5ec
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-21 13:22:51 -0300

    update cities controller to use City service as an class

--- controllers/cities_controller.js
+++ controllers/cities_controller.js
@@ -1,16 +1,16 @@
-const citiesService = require('../services/cities');
+const City = require('../services/cities');
 
 exports.index = (req, res, next) => {
     const query = req.query
     const onlyWithWeather = query.only_with_weather && JSON.parse(query.only_with_weather)
     return res.status(200).json({
-        cities: citiesService.findAll(onlyWithWeather)
+        cities: City.findAll(onlyWithWeather)
     })
 }
 
 exports.show = (req, res, next) => {
     const id = JSON.parse(req.params.id)
     return res.status(200).json({
-        city: citiesService.findById(id)
+        city: City.findById(id)
     })
 }
