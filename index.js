7812b3755ee3ca5017ff189288c9bfd4b27ce6e4
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-22 19:07:33 -0300

    Add SIGINT to shut down server on CTRL + C with docker

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const app = require('./server');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000
-const host = process.env.HOST || '127.0.0.1'
+const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host);
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT. Shuttinh down');
+  shutdown();
+});
+
+// shut down server
+function shutdown() {
+	process.exit();
+}
