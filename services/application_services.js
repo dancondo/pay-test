1f0a2b41caef9f060c7a87a0dd76b16a3123028d
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-21 22:01:44 -0300

    Application Service filter function accepts arrays in where params, when this happens, the function will call itself to resolve every condition

--- services/application_services.js
+++ services/application_services.js
@@ -2,6 +2,33 @@ const moment = require('moment');
 
 module.exports = class ApplicationServices {
 
+    static filter(array, where) {
+        if (where.constructor.name === 'Array') {
+            let reducedArray = array
+            for (let i = 0; i < where.length; i++) {
+                reducedArray = this.filter(reducedArray, where[i])
+            }
+            return reducedArray
+        }
+        return array.reduce((arr, item) => {
+            const newItem = item[where.op] ? item : new this(item)
+            if (where && !newItem[where.op]({
+                    ...where.params,
+                    adapter: where.adapter,
+                    val: newItem[where.attribute],
+                }))
+            {
+                return arr
+            }
+            arr.push(newItem)
+            return arr
+        }, [])
+    }
+
+    floatAdapter(arr) {
+        return arr.map(el => parseFloat(el))
+    }
+
     dataTimeAdapter(arr) {
         return arr.map(el => moment(el, 'YYYY/MM/DD').valueOf())
     }
