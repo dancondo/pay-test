506162fff3274edc3908e8f5a92f9f6e26f61e76
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-01-21 21:59:06 -0300

    Add latitude and longitude query params

--- controllers/cities_controller.js
+++ controllers/cities_controller.js
@@ -5,8 +5,39 @@ const City = require('../services/cities');
 exports.index = (req, res, next) => {
     const query = req.query
     const onlyWithWeather = query.only_with_weather && JSON.parse(query.only_with_weather)
+    const startLat = req.query.start_lat
+    const endLat = req.query.end_lat
+    const startLon = req.query.start_lon
+    const endLon = req.query.end_lon
+
+    const where = []
+
+    if (startLat && endLat) {
+        where.push({
+            op: 'inRange',
+            attribute: 'latitude',
+            adapter: 'floatAdapter',
+            params: {
+                maxVal: endLat,
+                minVal: startLat
+            }
+        })
+    }
+
+    if (startLon && endLon) {
+        where.push({
+            op: 'inRange',
+            attribute: 'longitude',
+            adapter: 'floatAdapter',
+            params: {
+                maxVal: endLon,
+                minVal: startLon
+            }
+        })
+    }
+
     return res.status(200).json({
-        cities: City.findAll(onlyWithWeather)
+        cities: City.findAll(onlyWithWeather, where)
     })
 }
 
